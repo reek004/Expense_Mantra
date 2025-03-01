3990834fa5419f25d9ad5f4bda4dd8b0d79d25ac
reek004 <chatterjeeswarnabha@gmail.com>
2025-03-02 01:12:34 +0530

    Add Express server setup with Apollo Server integration and user query resolver

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,44 @@
+import cors from 'cors';
+import http from 'http';
+import express from 'express';
+
+
+
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+
 
 import mergedResolvers from "./resolvers/merger.js";
 import mergedTypeDefs from "./typeDefs/merger.js";
 
+const app = express();
+// Our httpServer handles incoming requests to our Express app.
+// Below, we tell Apollo Server to "drain" this httpServer,
+// enabling our servers to shut down gracefully.
+const httpServer = http.createServer(app);
+
 const server = new ApolloServer({
 	typeDefs: mergedTypeDefs,
 	resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
-const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-  });
-  
-  console.log(`🚀  Server ready at: ${url}`);
+await server.start();
+
+
+app.use(
+  '/',
+  cors(),
+  express.json(),
+  // expressMiddleware accepts the same arguments:
+  // an Apollo Server instance and optional configuration options
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ req }),
+  }),
+);
+
+await new Promise((resolve) =>
+  httpServer.listen({ port: 4000 }, resolve),
+);
+console.log(`🚀 Server ready at http://localhost:4000/`);
